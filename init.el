bf574fcda4a9d1ef935e9d5da8ea67eaa32ac8cd
Ian J. Watson <ian.james.watson@cern.ch>
2024-01-04 02:27:02 +0900

    vterm, org, etc

--- init.el
+++ init.el
@@ -74,10 +74,10 @@
   (general-override-mode)
   (general-create-definer leader-def
     :keymaps 'override
+    :states '(normal visual insert emacs)
     :prefix "SPC"
     :non-normal-prefix "C-SPC")
   (leader-def
-   :states '(normal visual insert emacs)
    "SPC" 'execute-extended-command
    "TAB" 'switch-to-previous-buffer
    "f" 'find-file
@@ -90,12 +90,23 @@
    "g" 'magit-status
    "v" 'split-window-right
    "1" 'delete-other-windows
-   "-" (lambda () (interactive) (dired default-directory))))
+   "-" `(,(lambda () (interactive) (dired default-directory)) :which-key "dired")))
 
 (use-package dired
   :elpaca nil
   :init
   (setq dired-dwim-target t))
+
+(use-package dired-subtree
+  :custom
+  (dired-subtree-cycle-depth 2)
+  :bind (:map dired-mode-map
+              ("<backtab>" . dired-subtree-cycle)
+              ("<tab>" . dired-subtree-toggle)))
+
+(use-package peep-dired
+  :after dired
+  :bind (:map dired-mode-map ("I" . peep-dired)))
 
 (use-package which-key
   :custom
@@ -149,10 +160,117 @@
   :config
   (recentf-mode))
 
-(use-package vterm)
+(use-package s)
+(use-package vterm
+  :after s
+  ; :defines ijw-shell
+  :bind
+  ("C-c v" . ijw-shell)
+  :init
+  (defun ijw-shell ()
+    (interactive)
+    (let* ((buf-shorts
+            (seq-map (lambda (n) (s-replace ">" "" (s-replace "*vterm*<" "" n)))
+                     (seq-filter (lambda (n) (s-starts-with? "*vterm*" n))
+                                 (seq-map #'buffer-name (buffer-list)))))
+           (buf (completing-read "vterm name: " buf-shorts))
+           (name (concat "*vterm*<" buf ">")))
+      (if (get-buffer name)
+          (switch-to-buffer name)
+        (vterm name))))
+  (leader-def "t" 'ijw-shell))
 
 (use-package pdf-tools
   :config
   (pdf-tools-install))
+
+(use-package tree-sitter)
+(use-package tree-sitter-langs :after tree-sitter
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+
+;;; ORG
+
+(use-package org
+  :init
+  (leader-def
+   "n" '(:ignore t :which-key "org")
+   "nl" 'org-store-link
+   "nc" 'org-capture
+   "nb" 'org-iswitchb
+   "nn" 'org-agenda))
+
+(use-package org-journal
+  :after org
+  :ensure t
+  :custom
+  (org-journal-dir (expand-file-name "~/org-roam/daily"))
+  (org-journal-carryover-items "")
+  (org-journal-time-prefix "** ")
+  (org-journal-date-prefix "* ")
+  (org-journal-date-format "%A, %x")
+  (org-journal-time-format " ")
+  (org-journal-file-type 'weekly)
+  (org-journal-file-format "%Y_%m_%d.org")
+  ; (org-journal-enable-agenda-integration nil)
+  ; (org-journal-find-file #'find-file)
+  :init
+  (leader-def
+    "nj" 'org-journal-new-entry
+    "nd" 'org-journal-new-date-entry
+    "nt" 'org-journal-open-current-journal-file))
+
+(use-package jupyter
+  :config
+  (require 'ob-jupyter)
+  (require 'jupyter-python)
+  ;(use-package ob-mermaid)
+  (add-to-list 'org-src-lang-modes '("jupyter" . python))
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (python . t)
+     (shell . t)
+     (ditaa . t)
+     (dot . t)
+     (plantuml . t)
+     (gnuplot . t)
+     (org . t)
+     (scheme . t)
+     (latex . t)
+     ;(mermaid . t)
+     (jupyter . t))))
+;; workaround for jupyter nonsense...
+(setenv "PYDEVD_DISABLE_FILE_VALIDATION" "1")
+
+;;; END ORG
+
+(use-package emms
+  :config
+  (leader-def
+  "m m" 'emms-pause
+  "m s" 'emms-play-or-stop
+  "m i" 'emms-show
+  "m n" 'emms-next
+  "m p" 'emms-previous
+  "m d" 'emms-add-directory
+  "m f" 'emms-add-file
+  "m +" 'emms-volume-mode-plus
+  "m =" 'emms-volume-mode-plus
+  "m -" 'emms-volume-mode-minus
+  "m <" 'emms-seek-backward
+  "m >" 'emms-seek-forward
+  "m b" 'emms) ;; go to emms *b*uffer
+  (require 'emms-setup)
+  (defun emms-play-or-stop () (interactive)
+    (if emms-player-playing-p
+        (emms-stop) (emms-start)))
+  (emms-all)
+  (setq emms-volume-change-function 'emms-volume-pulse-change)
+  (setq emms-volume-mode-timeout 2)
+  (setq emms-player-list '(emms-player-vlc)
+        emms-info-functions '(emms-info-native)))
 
 (elpaca-wait)
